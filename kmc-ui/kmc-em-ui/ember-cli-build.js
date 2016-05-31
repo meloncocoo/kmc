50436476ac89a292afdd6ae35ca40fc381a883cc
Melon <meloncocoo@gmail.com>
2016-05-31 18:02:06 +0800

    +updated vendor

--- kmc-ui/kmc-em-ui/ember-cli-build.js
+++ kmc-ui/kmc-em-ui/ember-cli-build.js
@@ -20,7 +20,7 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
-    app.import('bower_components/jquery-mmenu/source/jquery.mmenu.css');
+//    app.import('bower_components/jquery-mmenu/source/jquery.mmenu.css');
     app.import('bower_components/jquery-mmenu/source/jquery.mmenu.js');
     app.import('bower_components/breakpoints.js/dist/breakpoints.js');
 
